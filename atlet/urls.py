2c9c4404494fcd2eb7fbd65429527924302f5df4
hayfa_najma <hayfanajmaa22@gmail.com>
2023-05-23 22:44:42 +0700

    update form kualifikasi

--- atlet/urls.py
+++ atlet/urls.py
@@ -3,7 +3,6 @@ from atlet.views import *
 
 app_name = 'atlet'
 urlpatterns = [
-    path('form_data_kualifikasi/', form_data_kualifikasi, name='form_data_kualifikasi'),
     path('pertanyaan_kualifikasi/', pertanyaan_kualifikasi, name='pertanyaan_kualifikasi'),
     path('daftar_event1/', daftar_event1, name='daftar_event1'),
     path('daftar_event2/', daftar_event2, name='daftar_event2'),
