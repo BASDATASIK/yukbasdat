cae9c2d1ccc37357903abefa3feb504089177ca0
Muhammad Nabiel Andityo <mnabielap@gmail.com>
2023-05-25 18:21:17 +0700

    add static to url

--- babadu/urls.py
+++ babadu/urls.py
@@ -41,6 +41,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -48,4 +50,4 @@ urlpatterns = [
     path('atlet/', include('atlet.urls')),
     path('pelatih/', include('pelatih.urls')),
     path('umpire/', include('umpire.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
